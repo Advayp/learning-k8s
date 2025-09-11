2e1c279cc357aabe89bd69c071ae18cf4cb5f99c
Advay Patil <advaypatil27@gmail.com>
2025-09-10 22:12:38 -0700

    Add makefile and use k8s config and secrets

--- hello-world/hello/main.go
+++ hello-world/hello/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/secrets", secrets)
 
 	http.ListenAndServe(":5051", nil)
 	fmt.Println("Server started on port 5051...")
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Hello, World\n")
+	message := os.Getenv("message")
+	fmt.Fprintf(w, "Message: %s\n", message)
+}
+
+func secrets(w http.ResponseWriter, req *http.Request) {
+	username := os.Getenv("username")
+	password := os.Getenv("password")
+	fmt.Fprintf(w, "Username: %s, Password: %s\n", username, password)
 }
